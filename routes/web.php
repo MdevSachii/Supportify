842d7282fecdc2cc120ea92e710b3eff5442d606
Sachini Wijesinghe <wijesinghesachini09@gmail.com>
2024-12-27 18:26:10 +0530

    feat(SP-02): Create customer and agent view

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,17 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AgentController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [CustomerController::class, 'index'])->name('customer');
+
+Route::prefix('customer')->group(function () {
+    //
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'verified'])->prefix('agent')->group(function () {
+    Route::get('/', [AgentController::class, 'index'])->name('agent');
 });
 
 require __DIR__.'/auth.php';
